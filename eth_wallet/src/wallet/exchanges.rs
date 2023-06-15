9c1a90071027613738a243c5b8877d604e085ef0
Ecmel Albayrak <ecmel.kytz@gmail.com>
2023-06-15 15:33:37 +0300

    Update exchanges.rs

--- eth_wallet/src/wallet/exchanges.rs
+++ eth_wallet/src/wallet/exchanges.rs
@@ -1,5 +1,6 @@
 use web3::types::U256;
 
+// Stolen from https://github.com/tmsdev82/rust-eth-crypto-wallet-tutorial/blob/main/src/utils.rs
 pub fn wei_to_eth(wei_val: U256) -> f64 {
   let wei = wei_val.as_u128() as f64;
   wei / 1_000_000_000_000_000_000.0
